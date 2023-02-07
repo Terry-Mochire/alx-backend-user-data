604579086d8ef3a89d5da52c07a09dcd8f657dec
Terry <terrybmochire@gmail.com>
2023-02-08 00:49:00 +0300

    Advanced task.

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -17,28 +17,29 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
-        Determine if a path should be authenticated
+        Determines whether a given path requires authentication or not
         Args:
-            path:
-            excluded_paths:
-
-        Returns:
-
+            - path(str): Url path to be checked
+            - excluded_paths(List of str): List of paths that do not require
+              authentication
+        Return:
+            - True if path is not in excluded_paths, else False
         """
-
-        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+        if path is None:
             return True
-
-        if path[-1] != '/' and path[-1] != '*':
-            path += '/'
-
-        for excluded_path in excluded_paths:
-            if excluded_path[-1] != '/' and excluded_path[-1] != '*':
-                excluded_path += '/'
-
-            if excluded_path == path:
-                return False
-
+        elif excluded_paths is None or excluded_paths == []:
+            return True
+        elif path in excluded_paths:
+            return False
+        else:
+            for i in excluded_paths:
+                if i.startswith(path):
+                    return False
+                if path.startswith(i):
+                    return False
+                if i[-1] == "*":
+                    if path.startswith(i[:-1]):
+                        return False
         return True
 
     def authorization_header(self, request=None) -> str:
