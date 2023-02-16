b1905787b93fe7a2b1f80a5e5bab8fce207fbcea
Terry <terrybmochire@gmail.com>
2023-02-16 22:25:08 +0300

    Task 19.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,7 +3,9 @@
 """
 Flask app
 """
-from flask import Flask, jsonify, request, abort, redirect
+from typing import Tuple
+
+from flask import Flask, jsonify, request, abort, redirect, Response
 from auth import Auth
 
 app = Flask(__name__)
@@ -72,15 +74,15 @@ def get_reset_password_token() -> str:
 
 
 @app.route('/reset_password', methods=['PUT'], strict_slashes=False)
-def update_password() -> str:
+def update_password() -> tuple[Response, int]:
     email = request.form.get('email')
     reset_token = request.form.get('reset_token')
     new_password = request.form.get('new_password')
     try:
         AUTH.update_password(reset_token, new_password)
-        return jsonify({"email": email, "message": "Password updated"}), 200
     except ValueError:
         abort(403)
+    return jsonify({"email": email, "message": "Password updated"}), 200
 
 
 if __name__ == "__main__":
