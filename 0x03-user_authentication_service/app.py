0228efb7b16ad341f7174c25fa28fc6c93902771
Terry <terrybmochire@gmail.com>
2023-02-16 22:04:21 +0300

    Task 11.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -26,7 +26,7 @@ def register():
         return jsonify({"message": "email already registered"}), 400
 
 
-@app.route('/sessions', methods=['POST'])
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
 def login() -> str:
     email = request.form.get('email')
     password = request.form.get('password')
