e862c781446b79a9eef1e87a0e859e52fe287976
Terry <terrybmochire@gmail.com>
2023-02-16 19:55:02 +0300

    Task 0.

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -27,10 +27,3 @@ class User(Base):
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
-
-    def __init__(self, email, hashed_password,
-                 session_id=None, reset_token=None):
-        self.email = (email,)
-        self.hashed_password = (hashed_password,)
-        self.session_id = (session_id,)
-        self.reset_token = reset_token
