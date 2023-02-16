99a325cf85b451bb9993764e18ba30637046bf92
Terry <terrybmochire@gmail.com>
2023-02-16 22:06:56 +0300

    Task 11.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -26,18 +26,22 @@ def register():
         return jsonify({"message": "email already registered"}), 400
 
 
-@app.route('/sessions', methods=['POST'], strict_slashes=False)
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
 def login() -> str:
-    email = request.form.get('email')
-    password = request.form.get('password')
-    valid_login = Auth.valid_login(email, password)
-    if valid_login:
-        session_id = Auth.create_session(email)
-        response = jsonify({"email": email, "message": "logged in"})
-        response.set_cookie("session_id", session_id)
-        return response
-    else:
+    """
+    Log in a user if the credentials provided are correct, and create a new
+    session for them.
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not Auth.valid_login(email, password):
         abort(401)
+
+    session_id = Auth.create_session(email)
+    resp = jsonify({"email": f"{email}", "message": "logged in"})
+    resp.set_cookie("session_id", session_id)
+    return resp
 
 
 if __name__ == "__main__":
