30bf9d121b7ed48b21d9f5a9ef8aabfb8fbcebde
Terry <terrybmochire@gmail.com>
2023-02-16 22:26:32 +0300

    Task 19.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -74,7 +74,7 @@ def get_reset_password_token() -> str:
 
 
 @app.route('/reset_password', methods=['PUT'], strict_slashes=False)
-def update_password() -> tuple[Response, int]:
+def update_password() -> str:
     email = request.form.get('email')
     reset_token = request.form.get('reset_token')
     new_password = request.form.get('new_password')
@@ -82,6 +82,7 @@ def update_password() -> tuple[Response, int]:
         AUTH.update_password(reset_token, new_password)
     except ValueError:
         abort(403)
+
     return jsonify({"email": email, "message": "Password updated"}), 200
 
 
